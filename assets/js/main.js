5312ca1c2efe36370210f0a7beda5b07ab973913
Enzo <enzorossi97@hotmail.com>
2022-08-15 19:32:30 -0300

    Fix html bugs

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,9 @@
 jQuery(function() {  
 
     $('#PetsCategoriesCarousel').carousel({ interval: 2000 });
+    $(window).on('resize', function() {
+        if ($("#Navbar").hasClass("show"))
+            $("#Navbar").removeClass("show");
+    }); 
+
 });
